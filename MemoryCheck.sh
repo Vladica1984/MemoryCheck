4804301cfa4783e85d6c82860d53ccbea4a85a2b
Dejan Prokić <dprokic@gmail.com>
2017-12-28 14:57:01 +0100

    html

--- MemoryCheck.sh
+++ MemoryCheck.sh
@@ -11,8 +11,13 @@ if [[ "$free" -le $low_memory_limit ]]; then
   echo "</p><p>" >> "$file"
   free -mt >> "$file"
   echo "</p>" >> "$file"
-  echo -e "<font color="red">Warning, server memory running low! Free memory: $free MB</font>" | mail -a 'Content-Type: text/html' -A "$file" -s "$Subject" vpopovic@tmns.com
-  echo -e "Warning, server memory running low! Free memory: $free MB" | mail -A "$file" -s "$Subject" vpopovic@tmns.com
+  echo -e "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+  <html xmlns=\"http://www.w3.org/1999/xhtml\">
+   <head>
+     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+       <title>Demystifying Email Design</title>
+         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>
+	 </head><body><font color=\"red\">Warning, server memory running low! Free memory: $free MB</font></body></html>" | mail -a "Content-Type: text/html" -A "$file" -s "$Subject" vpopovic@tmns.com
   rm -rf "$file"
 fi
 
