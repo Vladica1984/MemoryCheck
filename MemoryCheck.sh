269f0f79dd54e5da2ee876a900a3e16e5ad8ef19
Dejan Prokić <dejanp@nordeus.com>
2018-01-04 22:08:04 +0100

    Improvements

--- MemoryCheck.sh
+++ MemoryCheck.sh
@@ -2,40 +2,42 @@
 
 Subject=$(hostname)
 
+file="/tmp/top_proccesses_consuming_memory.txt"
+email="$1"
+freeMemTrigger="$2"
+
 free=$(free -mt | grep Mem | awk '{print $4}')
 
-if [[ "$free" -le 2000 ]]; then
+if [[ "$free" -le $freeMemTrigger ]]; then
+	printf "From: Vladimir <vpopovic@tmns.com>
+To: $email
+Subject: Warning email
+MIME-Version: 1.0
+Content-Type: text/html
+Content-Disposition: inline
+<html>
+<head><title>Warning email</title>
+</head>
+<body style=\"font: monospace;\">
+<pre style=\"color: red\">Warning, server memory running low! Free memory: $free MB</pre>
+<pre>
+Checking the running listeners and free memory
 
-printf "Warning, server memory running low! Free memory: $free MB\n" > ./top_proccesses_consuming_memory.txt
+With netstat -ltn we are checking running listeners:
 
-echo " " >>./top_proccesses_consuming_memory.txt
+$(netstat -ltn | grep 44)
 
-printf "Checking the running listeners and free memory\n"  >> ./top_proccesses_consuming_memory.txt
+Free memory is checked with free -mt command and bellow is current situation:
 
-echo " " >>./top_proccesses_consuming_memory.txt
+$(free -mt)
 
-printf "With netstat -ltn we are checking running listeners:\n"  >> ./top_proccesses_consuming_memory.txt
-
-echo " " >>./top_proccesses_consuming_memory.txt
-
-netstat -ltn | grep 631 >>./top_proccesses_consuming_memory.txt
-
-echo " " >>./top_proccesses_consuming_memory.txt
-
-printf "Free memory is checked with free -mt command and bellow is current situation:\n"  >> ./top_proccesses_consuming_memory.txt
-
-echo " " >>./top_proccesses_consuming_memory.txt
-
-free -mt >> ./top_proccesses_consuming_memory.txt
-
-echo " " >>./top_proccesses_consuming_memory.txt
-
-date "+%Y-%m-%d %H:%M:%S" >> ./top_proccesses_consuming_memory.txt
-
-cat ./top_proccesses_consuming_memory.txt | mail -s "$Subject" vpopovic@tmns.com
-
-rm -rf ./top_proccesses_consuming_memory.txt
-
+Current time: $(date '+%Y-%m-%d %H:%M:%S')
+</pre>
+</body>
+</html>
+	" | sendmail -t
+else
+	echo "OK"
 fi
 
 exit 0
